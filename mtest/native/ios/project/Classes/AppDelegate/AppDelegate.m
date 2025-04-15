680efa03c5a86d5fbb30db75282e2f2f2e5d8235
danteryung <dantexx@uracle.co.kr>
2025-04-15 10:33:27 +0900

    test

--- mtest/native/ios/project/Classes/AppDelegate/AppDelegate.m
+++ mtest/native/ios/project/Classes/AppDelegate/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"dantexxxx");
     
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
